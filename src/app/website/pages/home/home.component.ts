b34ab362707bba1444af3917e641a3804ecf91f2
toseefdar <toseefdar537@gmail.com>
2024-10-28 06:48:28 -0700

    addded special skills and projects section and implement routing

--- src/app/website/pages/home/home.component.ts
+++ src/app/website/pages/home/home.component.ts
@@ -1,14 +1,20 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 import { ServicesCardsComponent } from './components/services-cards/services-cards.component';
-
+import { SliderModule } from 'primeng/slider';
+import { FormsModule } from '@angular/forms';
+import { ProjectsCardsComponent } from './components/projects-cards/projects-cards.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NgOptimizedImage,ServicesCardsComponent],
+  imports: [NgOptimizedImage,ServicesCardsComponent,SliderModule,FormsModule,ProjectsCardsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+communication:number=75;
+leaderShip:number=70;
+teamWork:number=90;
+flexibility:number=80;
 
 }
