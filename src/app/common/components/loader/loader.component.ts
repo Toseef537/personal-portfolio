982acace1c6b8d4cf5cff23bfbb7bcdea0c2c6a5
toseefdar <toseefdar537@gmail.com>
2024-11-07 05:14:48 -0800

    added loader for navigation

---
+++ src/app/common/components/loader/loader.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'loader',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './loader.component.html',
+  styleUrl: './loader.component.scss'
+})
+export class LoaderComponent {
+  @Input() isLoading: boolean = false;
+}
